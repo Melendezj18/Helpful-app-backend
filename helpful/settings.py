b77a0ca079edb0f31630b412dab0e757db86275f
Jose <alex@MacBook-Pro.local>
2023-03-10 10:58:32 -0500

    setup changes

--- helpful/settings.py
+++ helpful/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8av(c75978)ot@=c5e=@ds6_-k+8_t%d&)y%uq^%!*_)v7bk-1'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
