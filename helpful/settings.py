b7e387bb2ca6ab01e2110f356e608c880bc3f58c
Jose <alex@MacBook-Pro.local>
2023-03-15 07:50:11 -0400

    actually fully fixed

--- helpful/settings.py
+++ helpful/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,5 +148,5 @@ AUTH_USER_MODEL = 'helpful_app.User'
 CORS_ORIGIN_WHITELIST =[
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://localhost:8000'
+    'http://localhost:8000',
 ]
